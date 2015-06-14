4e3f93bd608ce0bcd7023551f6f2b0bbc669d4e2
chathuranga <chathurangasandunkumara@gmail.com>
2015-06-15 00:44:06 +0530

    a

--- HackerRank/src/hackerrank/HackerRank.java
+++ HackerRank/src/hackerrank/HackerRank.java
@@ -19,7 +19,7 @@ public class HackerRank {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new UtopianTree();
+        
         
     }
 
